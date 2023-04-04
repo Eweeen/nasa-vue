9e035fd66adfd1ec59599efa4de4ebb3c79a1a22
Ewen35550 <ewen.herve.35550@gmail.com>
2023-04-04 21:40:12 +0200

    ajout de tailwind linter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,18 @@
-<script setup lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HomeView",
+  mounted() {
+    fetch("https://api.le-systeme-solaire.net/rest/bodies/")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+});
+</script>
 
 <template>
-  <main>Home</main>
+  <div>Home</div>
 </template>
+
+<style scoped></style>
