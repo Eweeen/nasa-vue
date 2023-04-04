ff9df6a6949829fde977219df4b53a9b1024933b
Ewen35550 <ewen.herve.35550@gmail.com>
2023-04-04 16:37:13 +0200

    init

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+    "@vue/eslint-config-prettier/skip-formatting"
+  ],
+  parserOptions: {
+    ecmaVersion: "latest"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: false,
+        endOfLine: "auto",
+        semi: true
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ["cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"],
+      extends: ["plugin:cypress/recommended"]
+    }
+  ]
+};
