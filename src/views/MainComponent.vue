4614ee1663638f8b1e2408f6490683d385943535
Ewen35550 <ewen.herve.35550@gmail.com>
2023-04-07 11:52:46 +0200

    ajout du composant principal et commun à toutes les pages

---
+++ src/views/MainComponent.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NasaHeader from "@/components/layouts/header/Header.vue";
+</script>
+
+<template>
+  <section class="min-h-screen bg-slate-900">
+    <NasaHeader></NasaHeader>
+    <div class="p-6 pt-20 md:pb-12 md:px-12 md:pt-24 lg:px-16 xl:px-20">
+      <RouterView />
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
