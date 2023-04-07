6e832a0520a00bcbac5799f37bbb6299d0ea4665
Ewen35550 <ewen.herve.35550@gmail.com>
2023-04-07 11:51:14 +0200

    création des routes fetch des données de l'api et envoies de celles-ci dans le store

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,61 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Main from "@/views/MainComponent.vue";
+import Home from "@/views/HomeView.vue";
+import Stars from "@/views/stars/StarsList.vue";
+import Star from "@/views/stars/StarView.vue";
+import SolarSystem from "@/views/SolarSystem.vue";
+import MyFavorites from "@/views/MyFavorites.vue";
+import { usePlanetsStore } from "@/stores";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
-      name: "home",
-      component: HomeView
+      name: "main",
+      component: Main,
+      children: [
+        {
+          path: "/",
+          name: "home",
+          component: Home
+        },
+        {
+          path: "/stars",
+          name: "stars",
+          component: Stars
+        },
+        {
+          path: "/stars/:id",
+          name: "star",
+          component: Star
+        },
+        {
+          path: "/solar-system",
+          name: "solar-system",
+          component: SolarSystem
+        },
+        {
+          path: "/favorites",
+          name: "favorites",
+          component: MyFavorites
+        }
+      ]
     }
   ]
+});
+
+router.beforeEach((to, from, next) => {
+  const store = usePlanetsStore();
+
+  if (store.getPlanets.length > 0) next();
+
+  fetch("https://api.le-systeme-solaire.net/rest/bodies/")
+    .then((response) => response.json())
+    .then((data) => {
+      store.setPlanets(data.bodies);
+      next();
+    });
 });
 
 export default router;
