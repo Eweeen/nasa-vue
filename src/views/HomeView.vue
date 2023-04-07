d7f768bedbad1ebbc3733501331dde9ec68f50a2
Ewen35550 <ewen.herve.35550@gmail.com>
2023-04-07 13:18:06 +0200

    Mise en forme de la page d'accueil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,33 @@
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import type { Planet } from "@/interfaces/planet.interface";
+import { usePlanetsStore } from "@/stores";
 
-export default defineComponent({
-  name: "HomeView",
-  mounted() {
-    fetch("https://api.le-systeme-solaire.net/rest/bodies/")
-      .then((response) => response.json())
-      .then((data) => console.log(data));
-  }
-});
+const store = usePlanetsStore();
+const planets: Planet[] = store.getSolarSystem;
 </script>
 
 <template>
-  <div>Home</div>
+  <h1 class="text-4xl font-bold text-red-500 text-center my-16">Bienvenue dans l'espace</h1>
+
+  <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-2 lg:gap-4">
+    <template v-for="p in planets" :key="p.name">
+      <RouterLink
+        :to="'/stars/' + p.id"
+        class="m-auto flex flex-col items-center gap-2 mb-4 cursor-pointer"
+      >
+        <div
+          class="hover:bg-gradient-to-br from-[#ff665c] to-[#7023cb] backdrop-blur-lg w-[150px] h-[150px] lg:w-[200px] lg:h-[200px] rounded-2xl flex justify-center items-center shadow-[inset_0_4px_6px_0_rgb(255,255,255,0.5),inset_0_-1px_6px_0_rgb(255,255,255,0.5)]"
+        >
+          <img
+            :src="`src/assets/icons/${p.id}.png`"
+            alt="image planète"
+            class="w-20 h-20 lg:w-24 lg:h-24"
+          />
+        </div>
+        <span class="text-xl font-semibold">{{ p.name }}</span>
+      </RouterLink>
+    </template>
+  </div>
 </template>
 
 <style scoped></style>
