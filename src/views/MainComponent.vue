11047eede456724b7afe0c26aac7f8e9ccc000ae
Ewen35550 <ewen.herve.35550@gmail.com>
2023-06-20 10:06:53 +0200

    refactor(NavBar) déplacement du fichier NavBar dans le dossier layouts

--- src/views/MainComponent.vue
+++ src/views/MainComponent.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import NavBar from "@/components/layouts/header/NavBar.vue";
+import NavBar from "@/components/layouts/NavBar.vue";
 import FooterComponent from "@/components/layouts/FooterComponent.vue";
 import Notifications from "@/components/Notifications.vue";
 </script>
 
 <template>
   <section class="min-h-screen bg-slate-900">
-    <NavBar></NavBar>
+    <NavBar />
     <div class="p-6 pt-20 md:pb-12 md:px-12 md:pt-24 lg:px-16 xl:px-20">
       <RouterView />
     </div>
