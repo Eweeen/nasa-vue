62ffd73530dedc8ef2b149d8def51dd33ce9b7f9
Ewen35550 <ewen.herve.35550@gmail.com>
2023-06-20 09:40:01 +0200

    refactor(Footer) changement du nom de l'api dans le footer et redirection en _blank

--- src/components/layouts/FooterComponent.vue
+++ src/components/layouts/FooterComponent.vue
@@ -1,6 +1,8 @@
 <template>
   <footer class="p-6 lg:px-20 bg-slate-800 flex justify-between">
     <p>Créer par moi</p>
-    <a href="https://api.le-systeme-solaire.net" class="underline">API: le-systeme-solaire</a>
+    <a href="https://api.le-systeme-solaire.net" target="_blank" class="underline">
+      le-systeme-solaire
+    </a>
   </footer>
 </template>
